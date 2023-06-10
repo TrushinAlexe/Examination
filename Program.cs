d51a312d82e41b7b617c8b58a3030ac351a15928
Aleksey A.A <c1d1o@yandex.ru>
2023-06-11 02:13:40 +0300

    добавил метод PrintArray

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿string[] array1 = new string[5] {"Hey", "23", "student", "geekbrains", "res"};
 string[] array2 = new string[array1.Length];
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
